72503a237be3a7487f802ef2aad1ee57e468556a
Patrick Kipsum <pat.koech01@gmail.com>
2023-08-24 00:37:58 -0700

    added remove_stack.c file

---
+++ remove_stack.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * remove_stack - subtracts
+ * @head: head of the stack
+ * @count: line counter
+ * Return: nothing
+ */
+void remove_stack(stack_t **head, unsigned int count)
+{
+	stack_t *_abs;
+	int i, j;
+
+	_abs = *head;
+	for (i = 0; _abs != NULL; i++)
+		_abs = _abs->next;
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short", count);
+		fclose(bus.f);
+		free(bus.con);
+		f_list(*head);
+		exit(EXIT_FAILURE);
+	}
+	_abs = *head;
+	j = _abs->next->m - _abs->m;
+	_abs->next->m = j;
+	*head = _abs->next;
+	free(_abs);
+}
